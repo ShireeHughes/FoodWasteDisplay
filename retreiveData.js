4a3bcc42f5149c834958d986dc2a3134dc357647
Shiree Hughes <shughes@Shirees-MacBook-Pro-2.local>
2017-01-03 10:57:10 -0500

    Commit Javascript
    A javascript function to test hosting javascript in github

---
+++ retreiveData.js
@@ -0,0 +1,8 @@
+$.ajax({
+  dataType: "jsonp",
+  url: 'http://portal-isense.fau.edu:8080/wrs/mongo?numSamples=2880',
+  }).done(function ( data ) {
+  // do my stuff
+  console.log('here');
+  console.log(data);
+});

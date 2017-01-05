67775e63ceda3f7aad22aff086962bd76303b5cc
Shiree Hughes <shughes@Shirees-MacBook-Pro-2.local>
2017-01-05 17:39:46 -0500

    fixed http vs https problem

--- retreiveData.js
+++ retreiveData.js
@@ -2,6 +2,6 @@ function retreiveData() {
  $.ajax({
   method:"GET",
   dataType: "jsonp",
-  url: 'http://portal-isense.fau.edu:8080/wrs/mongo?numSamples=2880',
+  url: 'https://portal-isense.fau.edu:8080/wrs/mongo?numSamples=2880',
   });
 }

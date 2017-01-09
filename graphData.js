ad7ef4730244433ef6aadea8fcf7f64d6ebacec3
Shiree Hughes <shughes@Shirees-MacBook-Pro-2.local>
2017-01-09 12:43:07 -0500

    actually fixed date

--- graphData.js
+++ graphData.js
@@ -62,7 +62,7 @@ function getColor(value) {
  */
 function graphDataMultiLine(max, datasets) {
    var today = new Date();
-   var yesterday = new Data(new Date().setHours(today.getHours() - 24));
+   var yesterday = new Date(new Date().setHours(today.getHours() - 24));
 console.log(yesterday);
 console.log(today);
 

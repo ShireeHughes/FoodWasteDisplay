ffd624dfad47bacc07910553609d7fce96d111b5
Shiree Hughes <shughes@Shirees-MacBook-Pro-2.local>
2017-01-19 12:07:03 -0500

    new GET functions from server

--- JavaScript/retreiveData.js
+++ JavaScript/retreiveData.js
@@ -1,7 +1,15 @@
-function retreiveData() {
+function getMaxMin() {
  $.ajax({
   method:"GET",
   dataType: "jsonp",
-  url: 'http://portal-isense.fau.edu:8080/wrs/mongo?numSamples=2880',
+  url: 'http://portal-isense.fau.edu:8080/wrs/mongo?ID=1',
+  });
+}
+
+function retrieveData() {
+ $.ajax({
+  method:"GET",
+  dataType: "jsonp",
+  url: 'http://portal-isense.fau.edu:8080/wrs/mongo?ID=0',
   });
 }
